7c5185f04e09c95dc21dc6e45d8222739abbf9b7
Chivy Lim <chivy@web-essentials.asia>
2020-02-28 11:13:00 +0700

    [IMP] Set max screen width for desktop

--- resources/js/layouts/Main.vue
+++ resources/js/layouts/Main.vue
@@ -1,30 +1,30 @@
 <template>
-    <div class="page-container">
-        <div class="container">
-            <div>
+    <div class="page-container md-layout md-alignment-top-center">
+        <div class="md-layout-item md-size-50 md-small-size-100">
+            <div class="header-container">
                 <md-autocomplete
                     v-model="selectedEmployee"
                     class="searchItem"
                     :md-options="employees"
                     md-layout="box"
-                    md-dense>
+                    md-dense
+                >
                     <label>Search ...</label>
                 </md-autocomplete>
             </div>
+
+            <slot></slot>
+
+            <div class="bottom-bar md-layout md-alignment-top-center">
+                <md-bottom-bar md-sync-route class="md-layout-item md-size-50 md-small-size-100">
+                    <md-bottom-bar-item to="/" exact md-label="Home" md-icon="home"></md-bottom-bar-item>
+                    <md-bottom-bar-item to="/feed" md-label="Feed" md-icon="favorite"></md-bottom-bar-item>
+                    <md-bottom-bar-item to="/sms" md-label="SMS" md-icon="textsms"></md-bottom-bar-item>
+                    <md-bottom-bar-item to="/cart" md-label="Cart" md-icon="shopping_cart"></md-bottom-bar-item>
+                </md-bottom-bar>
+            </div>
+
         </div>
-
-        <md-app>
-            <md-app-content>
-                <slot></slot>
-            </md-app-content>
-        </md-app>
-
-        <md-bottom-bar md-sync-route class="bottom-bar">
-            <md-bottom-bar-item to="/" exact md-label="Home" md-icon="home"></md-bottom-bar-item>
-            <md-bottom-bar-item to="/feed" md-label="Feed" md-icon="favorite"></md-bottom-bar-item>
-            <md-bottom-bar-item to="/sms" md-label="SMS" md-icon="textsms"></md-bottom-bar-item>
-            <md-bottom-bar-item to="/cart" md-label="Cart" md-icon="shopping_cart"></md-bottom-bar-item>
-        </md-bottom-bar>
     </div>
 </template>
 
@@ -53,7 +53,11 @@
   }
 </script>
 <style lang="scss" scoped>
-    .container {
+    .page-container {
+        background-color: #ffffff;
+        padding-bottom: 56px;
+    }
+    .header-container {
         max-width: 600px;
         margin: 0 auto;
         padding: 15px 30px;
@@ -72,5 +76,8 @@
     .bottom-bar {
         position: fixed;
         bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: 9999;
     }
 </style>
